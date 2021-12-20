5650dd46585ab7590ec129bb9f650582854b42f5
Thorben Günther <admin@xenrox.net>
2021-12-20 19:07:32 +0100

    config: Add option to grab token from command output

--- config.go
+++ config.go
@@ -5,7 +5,9 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"strings"
 
 	"git.sr.ht/~emersion/go-scfg"
 	"git.sr.ht/~emersion/gqlclient"
@@ -38,16 +40,31 @@ func createClient(service string) *Client {
 	if len(inst.Params) == 0 {
 		log.Fatalf("missing instance hostname")
 	}
+
+	var token string
 	accessToken := inst.Children.Get("access-token")
 	if accessToken == nil || len(accessToken.Params) == 0 {
-		log.Fatalf("missing instance access-token")
+		tokenCmd := inst.Children.Get("access-token-cmd")
+		if tokenCmd == nil || len(tokenCmd.Params) == 0 {
+			log.Fatalf("missing instance access-token or access-token-cmd")
+		}
+
+		cmd := exec.Command(tokenCmd.Params[0], tokenCmd.Params[1:]...)
+		output, err := cmd.Output()
+		if err != nil {
+			log.Fatalf("could not execute access-token-cmd: %v", err)
+		}
+
+		token = strings.Fields(string(output))[0]
+	} else {
+		token = accessToken.Params[0]
 	}
 
 	hostname := inst.Params[0]
 	baseURL := fmt.Sprintf("https://%s.%s", service, hostname)
 	gqlEndpoint := baseURL + "/query"
 
-	tokenSrc := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: accessToken.Params[0]})
+	tokenSrc := oauth2.StaticTokenSource(&oauth2.Token{AccessToken: token})
 	httpClient := oauth2.NewClient(context.Background(), tokenSrc)
 	return &Client{
 		Client:  gqlclient.New(gqlEndpoint, httpClient),
