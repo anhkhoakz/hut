0c8c36c2e1f3910d6afb3b6ab9e2073cacf644e7
Simon Ser <contact@emersion.fr>
2023-06-20 01:26:15 +0200

    contrib: Add script to update schemas
    References: https://lists.sr.ht/~emersion/hut-dev/patches/40334
    
    Co-authored-by: Thorben Günther <admin@xenrox.net>

---
+++ contrib/update_schemas.sh
@@ -0,0 +1,18 @@
+#!/bin/sh -eu
+
+set -- builds git lists meta pages paste todo
+for service in "$@"; do
+    url="https://git.sr.ht/~sircmpwn/$service.sr.ht"
+    dir="$service"srht
+
+    api_dir="/api"
+    if [ "$service" = pages ]; then
+        api_dir=""
+    fi
+
+    tag=$(git -c 'versionsort.suffix=-' ls-remote --refs --sort='version:refname' --tags "$url" |
+        tail --lines=1 |
+        cut --delimiter='/' --fields=3)
+    echo "$url/blob/$tag$api_dir/graph/schema.graphqls"
+    curl -f -o "srht/$dir/schema.graphqls" "$url/blob/$tag$api_dir/graph/schema.graphqls"
+done
