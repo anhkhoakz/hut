3c829fa3748f25d6e4ffe86dc11289272150fe93
Thorben Günther <admin@xenrox.net>
2023-06-22 12:28:06 +0200

    export/git: Add pagination

--- export/git.go
+++ export/git.go
@@ -49,45 +49,53 @@ func (ex *GitExporter) Export(ctx context.Context, dir string) error {
 	}
 	sshUser := settings.Settings.SshUser
 
-	repos, err := gitsrht.Repositories(ex.client, ctx, nil)
-	if err != nil {
-		return err
-	}
-
 	baseURL, err := url.Parse(ex.BaseURL())
 	if err != nil {
 		panic(err)
 	}
 
-	// TODO: Should we fetch & store ACLs?
-	for _, repo := range repos.Results {
-		repoPath := path.Join(dir, "repos", repo.Name)
-		cloneURL := fmt.Sprintf("%s@%s:%s/%s", sshUser, baseURL.Host, repo.Owner.CanonicalName, repo.Name)
-		if _, err := os.Stat(repoPath); err == nil {
-			log.Printf("\tSkipping %s (already exists)", repo.Name)
-			continue
-		}
-
-		log.Printf("\tCloning %s", repo.Name)
-		cmd := exec.Command("git", "clone", "--mirror", cloneURL, repoPath)
-		if err := cmd.Run(); err != nil {
-			return err
-		}
-
-		repoInfo := GitRepoInfo{
-			Name:        repo.Name,
-			Description: repo.Description,
-			Visibility:  repo.Visibility,
-		}
-
-		file, err := os.Create(path.Join(repoPath, "srht.json"))
+	var cursor *gitsrht.Cursor
+	for {
+		repos, err := gitsrht.Repositories(ex.client, ctx, cursor)
 		if err != nil {
 			return err
 		}
-		err = json.NewEncoder(file).Encode(&repoInfo)
-		file.Close()
-		if err != nil {
-			return err
+
+		// TODO: Should we fetch & store ACLs?
+		for _, repo := range repos.Results {
+			repoPath := path.Join(dir, "repos", repo.Name)
+			cloneURL := fmt.Sprintf("%s@%s:%s/%s", sshUser, baseURL.Host, repo.Owner.CanonicalName, repo.Name)
+			if _, err := os.Stat(repoPath); err == nil {
+				log.Printf("\tSkipping %s (already exists)", repo.Name)
+				continue
+			}
+
+			log.Printf("\tCloning %s", repo.Name)
+			cmd := exec.Command("git", "clone", "--mirror", cloneURL, repoPath)
+			if err := cmd.Run(); err != nil {
+				return err
+			}
+
+			repoInfo := GitRepoInfo{
+				Name:        repo.Name,
+				Description: repo.Description,
+				Visibility:  repo.Visibility,
+			}
+
+			file, err := os.Create(path.Join(repoPath, "srht.json"))
+			if err != nil {
+				return err
+			}
+			err = json.NewEncoder(file).Encode(&repoInfo)
+			file.Close()
+			if err != nil {
+				return err
+			}
+		}
+
+		cursor = repos.Cursor
+		if cursor == nil {
+			break
 		}
 	}
 
