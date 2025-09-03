888ebb94f002d912739bf49a9dc86e07e16fdc24
Ramon Rodrigues <sir@ramonrodrigu.es>
2025-09-03 09:04:35 -0300

    Add support for --web flag on android

--- main.go
+++ main.go
@@ -264,6 +264,8 @@ func openURL(url string) error {
 		return runCmd("open", url)
 	case "linux":
 		return linux(url)
+	case "android":
+		return android(url)
 	case "netbsd":
 		return netbsd(url)
 	case "openbsd":
@@ -281,6 +283,18 @@ func linux(url string) error {
 	// There are multiple possible providers to open a browser on linux
 	// One of them is xdg-open, another is x-www-browser, then there's www-browser, etc.
 	// Look for one that exists and run it
+	for _, provider := range providers {
+		if _, err := exec.LookPath(provider); err == nil {
+			return runCmd(provider, url)
+		}
+	}
+
+	return &exec.Error{Name: strings.Join(providers, ","), Err: exec.ErrNotFound}
+}
+
+func android(url string) error {
+	providers := []string{"termux-open", "xdg-open"}
+
 	for _, provider := range providers {
 		if _, err := exec.LookPath(provider); err == nil {
 			return runCmd(provider, url)
